936880d7e116f7fa141b9145100d95e3101d7619
andrebreton <andrebreton@live.com>
2017-09-19 16:03:05 -0700

    Initial Commit

---
+++ Sieve of Eratosthenes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sieve of Eratosthenes
+//
+//  Created by Admin on 9/19/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
