f1b378dc2c9930ca0998a0796bf3a7eb58c115c7
andrebreton <andrebreton@live.com>
2017-09-19 16:06:46 -0700

    Created Code based on the Sieve of Eratosthenes

--- Sieve of Eratosthenes/main.swift
+++ Sieve of Eratosthenes/main.swift
@@ -1,12 +1,64 @@
 //
 //  main.swift
-//  Sieve of Eratosthenes
+//  exercises
 //
-//  Created by Admin on 9/19/17.
+//  Created by Andre on 9/19/17.
 //  Copyright © 2017 Admin. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ^^ : PowerPrecedence
+func ^^ (radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
+}
+
+func calculate (primes : [Bool] , N : Int) -> [Bool] {
+    let N = N
+    var primes = primes
+    for i in 2...Int(sqrt(Double(N))) {
+        if primes[i] == true {
+            for j in 0...N {
+                let calculation = (i ^^ 2) + (j * i)
+                if calculation < N {
+                    primes[calculation] = false
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return primes
+}
+
+
+let N = 100
+
+var primes = [Bool](repeating: true, count: N)
+
+primes = calculate(primes: primes, N: N)
+
+for index in 2...N-1 {
+    if primes[index] == true {
+        print(index)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
